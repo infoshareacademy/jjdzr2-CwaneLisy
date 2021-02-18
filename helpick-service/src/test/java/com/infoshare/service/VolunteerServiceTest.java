eb2baf266b009c1ae0d3d6900b4a772890298395
magkamon <kamonmagdalena@gmail.com>
2021-02-18 17:14:09 +0100

    [JJDZR2CL-50] tests for method edit volunteer added

--- helpick-service/src/test/java/com/infoshare/service/VolunteerServiceTest.java
+++ helpick-service/src/test/java/com/infoshare/service/VolunteerServiceTest.java
@@ -4,12 +4,14 @@ import com.infoshare.database.DB;
 import com.infoshare.domain.TypeOfHelp;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.infoshare.domain.Volunteer;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
 import java.util.UUID;
 
 class VolunteerServiceTest {
@@ -35,5 +37,28 @@ class VolunteerServiceTest {
         //Then
         assertThat(isNewVolunteerRegistered).isFalse();
     }
+
+   @Test
+   public void testEditVolunteerDataForUuidInDb(){
+       //Given
+       UUID testUuid = UUID.randomUUID();
+       Volunteer volunteerInDb = new Volunteer( "Janko","Gdansk", "janko@gmail.com", "123654789", TypeOfHelp.SHOPPING, true, testUuid);
+       when(db.getVolunteer(testUuid)).thenReturn(Optional.of(volunteerInDb));
+       //When
+       boolean isEditionSuccessful = volunteerService.editVolunteerData("Franko","Gdansk", "franko@gmail.com", "123654789", TypeOfHelp.SHOPPING, true, testUuid);
+       //Then
+       assertThat(isEditionSuccessful).isTrue();
+   }
+
+   @Test
+    public void testEditVolunteerDataForUuidNotSavedInDb(){
+        //Given
+       when(db.getVolunteer((UUID) any())).thenReturn(Optional.empty());
+       //When
+       boolean isEditionSuccessful = volunteerService.editVolunteerData("Franko","Gdansk", "franko@gmail.com", "123654789", TypeOfHelp.SHOPPING, true, UUID.randomUUID());
+       //Then
+       assertThat(isEditionSuccessful).isFalse();
+   }
+
 
 }
