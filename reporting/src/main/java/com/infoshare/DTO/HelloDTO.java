e419b95f2961f5076479d949edb8feac0ba67d76
magkamon <kamonmagdalena@gmail.com>
2021-04-03 19:37:39 +0200

    creating new reporting module

---
+++ reporting/src/main/java/com/infoshare/DTO/HelloDTO.java
@@ -0,0 +1,14 @@
+package com.infoshare.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloDTO {
+
+    String welcomeMessage;
+}
---
+++ reporting/src/main/java/com/infoshare/DTO/ResponseDTO.java
@@ -0,0 +1,15 @@
+package com.infoshare.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO {
+
+    String response;
+
+}
